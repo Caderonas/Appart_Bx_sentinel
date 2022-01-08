bb1ebbc40ea6dbfd3d5190839454aa3b15ba636f
Caderonas <loicver1997@gmail.com>
2022-01-08 13:12:56 +0100

    add date

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Appart_Bx_sentinel
+# Appart Bx sentinel
+A simple app to find all appartement in a budget with an argument
+## Install dependencies
+`pip3 install -U selenium webdriver_manager bs4 lxml`
+## How to run
+To use the code you just have to run it with 1 argument, which is the max price that you allow to your research.
+`python3 ./appart.py MAX_PRICE `
--- appart.py
+++ appart.py
@@ -5,11 +5,7 @@ from webdriver_manager.firefox import GeckoDriverManager
 from bs4 import BeautifulSoup
 import argparse
 import json
-
-'''
-pip3 install -U selenium
-pip3 install webdriver_manager
-'''
+from datetime import date
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Search appart')
@@ -22,9 +18,10 @@ if __name__ == "__main__":
     opt.add_argument("--headless")
     driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options = opt)
 
-
+    DATE_OF_MOVING_IN = date.today()
+    
     # SPOTAHOME
-    driver.get(f"https://www.spotahome.com/s/brussels/for-rent:apartments/for-rent:rooms/for-rent:studios/bedrooms:1/bedrooms:2?move-in=2022-01-06&budget=0-"+str(args.integers))
+    driver.get(f"https://www.spotahome.com/s/brussels/for-rent:apartments/for-rent:rooms/for-rent:studios/bedrooms:1/bedrooms:2?move-in="+str(DATE_OF_MOVING_IN)+"&budget=0-"+str(args.integers))
     
     soup = BeautifulSoup(driver.page_source, "lxml")
 
@@ -39,6 +36,21 @@ if __name__ == "__main__":
             "img" : appart.find("img")['src'],
             "link" : appart.find("a", class_="home-card__HomecardWrapper___OmV4c")['href']
         }
-
+        print(appart)
+    '''
+    ### WORKING ON ###
     # IMMOWEB
     driver.get(f"https://www.immoweb.be/en/search/house-and-apartment/for-rent/Brussels%20City/1000?countries=BE&maxPrice="+str(args.integers)+"&isImmediatelyAvailable=true&page=1&orderBy=relevance")
+
+    soup = BeautifulSoup(driver.page_source, "lxml")
+
+    for appart in soup.find_all("li", class_="search-results__item"):
+        if appart.find("div", class_="adfocus__media") is None:
+            appart = {
+                "price" : appart.find("span").text,
+                "type" : appart.find("a").text,
+                "img" : appart.find("img")['src'],
+                "link" : appart.find("a")['href']
+            }
+            print(appart)
+    '''
